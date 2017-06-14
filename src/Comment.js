19c9bcbf53e59a954645bfe382b7ef0a3a06675a
Roman <kaboom.ua@gmail.com>
2017-06-14 18:45:34 +0300

    Create Comment.js

--- src/Comment.js
+++ src/Comment.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
 export default (props) => (
+	//li сильно уменьшает возможность переиспользовать в будущем, лучше вынести его в Comments
 	<li>
 		<h3>{props.comment.user}</h3>
 		<p>{props.comment.text}</p>
