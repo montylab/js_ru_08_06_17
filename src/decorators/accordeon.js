939d3c4e0ff2d0ba0eb47acf8f28620527034eb8
Roman <kaboom.ua@gmail.com>
2017-06-19 12:11:44 +0300

    Update accordeon.js

--- src/decorators/accordeon.js
+++ src/decorators/accordeon.js
@@ -1,5 +1,6 @@
 import React, {Component as ReactComponent} from 'react'
 
+//WrappedComponent я называл чтоб легче понять было. Лучше выбирай более значущее название
 export default (OriginalComponent) => class WrappedAccordeonComponent extends ReactComponent {
     state = {
         openId: null
