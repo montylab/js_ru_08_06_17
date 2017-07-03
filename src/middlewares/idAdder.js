29bd137bcbf16fdf471812ba53ce45daa3e20110
Roman <kaboom.ua@gmail.com>
2017-07-03 10:39:32 +0300

    Update idAdder.js

--- src/middlewares/idAdder.js
+++ src/middlewares/idAdder.js
@@ -1,6 +1,7 @@
 import { ADD_COMMENT } from '../constants.js'
 
 export default store => next => action => {
+	//через мидлвары будут проходить все экшины, суть в том, что делать их нужно максимально реюзабильными. Не завязывайся на один экшин
 	if (action.type === ADD_COMMENT) {
 		action.payload.id = (Math.random() * 1e9).toString(32)
 	}
